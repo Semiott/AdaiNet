84a1a80c24ff5f4a50748b9e548096ae74f0d2c8
Gokul Alex <gokulsan@users.noreply.github.com>
2021-03-21 18:21:00 +0530

    Create MintyManager.sol

---
+++ minty/contracts/MintyManager.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.6.8;
+
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/access/Ownable.sol";
+
+contract MintyManager is Ownable {
+    // @notice Transfer specified NFT to 
+    function sendNFT(IERC721 nftContract, uint tokenId, address recipient) external {
+        nftContract.transferFrom(address(this), recipient, tokenId);
+    }
+}
