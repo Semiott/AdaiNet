ad0a85d6867dd516837f22b4f34a09c8f92aca8f
Gokul Alex <gokulsan@users.noreply.github.com>
2021-03-21 20:18:38 +0530

    Change of Version to ^0.8.0

--- core/contracts/IERC1620.sol
+++ core/contracts/IERC1620.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache License 2.0
 
-pragma solidity >=0.6.0 <0.8.0;
+pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 /// @title ERC-1620 Money Streaming Standard
